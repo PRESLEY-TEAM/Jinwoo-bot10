5ff07734681c83a5b83889c516bfc10d8e6dbe4a
Elvis Presley <151203566+elvispresl@users.noreply.github.com>
2025-06-23 11:47:12 +0300

    ../

--- settings.js
+++ settings.js
@@ -17,7 +17,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'jinwoo:~xy8n1ZaC#gXFzLHip9KIPr6qA6JHeyJ-_2XuB3A8WyCjN6TtEbGI' 
 //Enter your Jinwoo session id here; must start with jinwoo:~
 
 //--------------------[ BOT NAME ]----------------------//
